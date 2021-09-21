1938ded042ed2740d859c89a4eee6f7375b35f5c
NewGabrielFroes <gabriel.froes32@gmail.com>
2021-09-21 09:46:37 -0300

    trabalhando com classes

---
+++ front_end/javascript_orientado_objetos/index.js
@@ -0,0 +1,17 @@
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+    rg;
+}
+
+const cliente1 = new Cliente()
+
+cliente1.nome = "Ricardo"
+cliente1.cpf = 8883832938493
+cliente1.agencia = 45673
+cliente1.saldo = 4500.32
+cliente1.rg = 48083434
+
+console.log(cliente1)
