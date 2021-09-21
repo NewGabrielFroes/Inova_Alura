4678efca11ccc2d16e434a7aa5bcf753e050e1fc
NewGabrielFroes <gabriel.froes32@gmail.com>
2021-09-21 10:25:12 -0300

    trabalhando com atributos privados

--- front_end/javascript_orientado_objetos/index.js
+++ front_end/javascript_orientado_objetos/index.js
@@ -3,36 +3,44 @@ class Cliente{
     cpf;
 }
 
-class ContaCorrente {
+class ContaCorrente{
     agencia;
-    saldo;
+     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo = 0;
 
     sacar(valor){
-        if (this.saldo >= valor){
-            this.saldo -= valor
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
         }
+    }
+
+    depositar(valor){
+        if(valor <= 0)
+        {
+            return;
+        } 
+        this._saldo += valor;           
     }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
+
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0;
-contaCorrenteRicardo.agencia = 450;
+contaCorrenteRicardo.agencia = 1001;
 
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.saldo = 100;
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.sacar(50);
+contaCorrenteRicardo.depositar(-100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
 
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
 
-console.log(contaCorrenteRicardo.saldo);
-console.log(cliente1);
-console.log(cliente2);
+console.log(contaCorrenteRicardo);
