b11daa2f0aa2c9b65a409ba4c31ece6ab05bb528
NewGabrielFroes <gabriel.froes32@gmail.com>
2021-09-30 16:11:48 -0300

    trabalhando com heranca

--- front_end/javascript_interfaces_heranca/Conta.js
+++ front_end/javascript_interfaces_heranca/Conta.js
@@ -1,44 +1,39 @@
 export class Conta {
-    constructor(saldoInicial, cliente, agencia){
-        this._saldoInicial = saldoInicial
+    constructor(saldo, cliente, agencia){
+        this._saldo = saldo
         this._cliente = cliente
         this._agencia = agencia
     }
 
     set cliente(novoValor){
         if(novoValor instanceof Cliente){
-            this._cliente = novoValor;
+            this._cliente = novoValor
         }
     }
 
     get cliente(){
-        return this._cliente;
+        return this._cliente
     }
 
     get saldo(){
-        return this._saldo;
+        return this._saldo
     }
 
     sacar(valor){
-        taxa = 1.1 * valor;
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
+        let taxa = 1
+        const valorSacado = taxa * valor
+        if(this._saldo >= valorSacado){
+            this._saldo -= valorSacado
+            return valorSacado
         }
     }
 
     depositar(valor){
-        if(valor <= 0)
-        {
-            return;
-        } 
         this._saldo += valor;           
     }
 
     tranferir(valor, conta){
-        
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
-        
     }
 }
