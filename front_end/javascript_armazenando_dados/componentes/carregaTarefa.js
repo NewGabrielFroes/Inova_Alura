c986772cb4bae2b12e4205fd3589fab3122818e5
Gabriel Froes <79543726+NewGabrielFroes@users.noreply.github.com>
2021-10-13 17:57:38 -0300

    remove e ordena datas repetidas

--- front_end/javascript_armazenando_dados/componentes/carregaTarefa.js
+++ front_end/javascript_armazenando_dados/componentes/carregaTarefa.js
@@ -1,11 +1,15 @@
-import { Tarefa } from "./criaTarefa.js"
+import { ordenaDatas, removeDatasRepetidas } from "../service/data.js"
+import { criaData } from "./criaData.js"
 
 export const carregaTarefa = () => {
     const lista = document.querySelector('[data-list]')
 
     const tarrefasCadastradas = JSON.parse(localStorage.getItem('tarefas')) || []
     
-    tarrefasCadastradas.forEach((tarefa) => {
-        lista.appendChild(Tarefa(tarefa))
+    lista.innerHTML = ''
+    const datasUnicas = removeDatasRepetidas(tarrefasCadastradas)
+    ordenaDatas(datasUnicas)
+    datasUnicas.forEach((dia) => {
+        lista.appendChild(criaData(dia))
     })
 }
