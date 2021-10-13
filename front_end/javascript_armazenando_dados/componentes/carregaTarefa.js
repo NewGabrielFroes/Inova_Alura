8f83dca0540386fdfcf9b5f01c5403b87075d6e2
Gabriel Froes <79543726+NewGabrielFroes@users.noreply.github.com>
2021-10-13 18:45:38 -0300

    módulo finalizado

--- front_end/javascript_armazenando_dados/componentes/carregaTarefa.js
+++ front_end/javascript_armazenando_dados/componentes/carregaTarefa.js
@@ -1,15 +1,15 @@
 import { ordenaDatas, removeDatasRepetidas } from "../service/data.js"
 import { criaData } from "./criaData.js"
-
-export const carregaTarefa = () => {
+export const carregaTarefa = () => { 
     const lista = document.querySelector('[data-list]')
 
-    const tarrefasCadastradas = JSON.parse(localStorage.getItem('tarefas')) || []
-    
-    lista.innerHTML = ''
-    const datasUnicas = removeDatasRepetidas(tarrefasCadastradas)
-    ordenaDatas(datasUnicas)
-    datasUnicas.forEach((dia) => {
+    const tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas'))||[]
+
+    lista.innerHTML = " "
+    const dataUnicas = removeDatasRepetidas(tarefasCadastradas)
+    ordenaDatas(dataUnicas)
+    dataUnicas.forEach((dia) => {
+        
         lista.appendChild(criaData(dia))
     })
 }
