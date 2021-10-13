06308a3a097e457e7c88907d01610f772b49ec06
Gabriel Froes <79543726+NewGabrielFroes@users.noreply.github.com>
2021-10-13 14:59:15 -0300

    usando localstorage

--- front_end/javascript_armazenando_dados/main.js
+++ front_end/javascript_armazenando_dados/main.js
@@ -22,7 +22,7 @@ const handleNovoItem = (evento) => {
     const criaTarefa = criarTarefa(dados)
     lista.appendChild(criaTarefa)
 
-    sessionStorage.setItem("tarefas", JSON.stringify(dados))
+    localStorage.setItem("tarefas", JSON.stringify(dados))
 
     input.value = " "
 }
