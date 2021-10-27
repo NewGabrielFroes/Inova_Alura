8b84acee705ea18668d6c21ce772b9c9ffbf8eba
Gabriel Froes <79543726+NewGabrielFroes@users.noreply.github.com>
2021-10-27 16:22:46 -0300

    módulo finalizado

--- front_end/javascript_assincrono/service/cliente-service.js
+++ front_end/javascript_assincrono/service/cliente-service.js
@@ -1,13 +1,16 @@
-const listaClientes = () => {
+const listaClientes = () =>  {
     return fetch(`http://localhost:3000/profile`)
     .then(resposta => {
-        return resposta.json()
+        if(resposta.ok){
+            return resposta.json()
+        }
+        throw new Error('Não foi possível listar os clientes')
     })
 }
 
-const criaCliente = (nome, email) => {
+const criaCliente = (nome, email) => { 
     return fetch(`http://localhost:3000/profile`, {
-        method: "POST",
+        method: 'POST', 
         headers: {
             'Content-Type' : 'application/json'
         },
@@ -16,19 +19,60 @@ const criaCliente = (nome, email) => {
             email: email
         })
     })
-    .then(resposta => {
-        return resposta.body
+    .then( resposta => {
+        if(resposta.ok){
+            return resposta.body
+        }
+        throw new Error('Não foi possível criar um cliente')
     })
 }
 
-const removeCliente = (id) => {
+const removeCliente = (id) => { 
     return fetch(`http://localhost:3000/profile/${id}`, {
-        method: "DELETE"
+        method: 'DELETE'
+    })
+    .then( resposta => { 
+        if(!resposta.ok){
+        throw new Error('Não foi possível deletar um cliente')
+        }
+    })
+}
+ 
+const detalhaCliente = (id) => { 
+    return fetch(`http://localhost:3000/profile/${id}`)
+    .then(resposta => { 
+        if(resposta.ok){
+            return resposta.json()
+        }
+    
+        throw new Error('Não foi possível detalhar um cliente')
     })
 }
 
-export const clienteService = {
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: { 
+            'Content-type' : 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome, 
+            email: email
+        })
+    })
+    .then( resposta => {
+        if(resposta.ok){
+            return resposta.json()
+        }
+        throw new Error('Não foi possível detalhar um cliente')
+    })
+}
+
+export const clienteService = { 
     listaClientes,
-    criaCliente,
-    removeCliente
+    criaCliente, 
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
+
 }
