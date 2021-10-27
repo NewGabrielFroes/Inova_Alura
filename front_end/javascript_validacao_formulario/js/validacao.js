8292b31d64d724f6be8090b1ed084e5fdac581e4
Gabriel Froes <79543726+NewGabrielFroes@users.noreply.github.com>
2021-10-27 17:53:39 -0300

    tratamento de erros

--- front_end/javascript_validacao_formulario/js/validacao.js
+++ front_end/javascript_validacao_formulario/js/validacao.js
@@ -1,8 +1,42 @@
-const dataNascimento = document.querySelector('#nascimento')
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo
 
-dataNascimento.addEventListener('blur', (evento) => {
-    validaDataNascimento(evento.target)
-})
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
+
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+    }
+    else {
+        input.parentElement.classList.add('input-container--invalido')
+    }
+}
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo não pode estar vazio.'
+    },
+    email: {
+        valueMissing: 'O campo de email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é válido.',
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode estar vazio.',
+        patternMismatch: `A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula,
+        um número e não deve conter símbolos.`
+    },
+    dataNascimento: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior de 18 anos para se cadastar.'
+    }
+
+    
+}
+
+const validadores = {
+    dataNascimento: input => validaDataNascimento(input)
+}
 
 function validaDataNascimento(input) {
     const dataRecebida = new Date(input.value)
