d6e54e964f2759a33a17cf554cba4d4c03f3fff1
Gabriel Froes <79543726+NewGabrielFroes@users.noreply.github.com>
2021-10-28 16:00:08 -0300

    customizando mensagens de erro

--- front_end/javascript_validacao_formulario/js/validacao.js
+++ front_end/javascript_validacao_formulario/js/validacao.js
@@ -7,15 +7,24 @@ export function valida(input) {
 
     if(input.validity.valid) {
         input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
     }
     else {
         input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
     }
 }
 
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
+
 const mensagensDeErro = {
     nome: {
-        valueMissing: 'O campo não pode estar vazio.'
+        valueMissing: 'O campo de nome não pode estar vazio.'
     },
     email: {
         valueMissing: 'O campo de email não pode estar vazio.',
@@ -36,6 +45,16 @@ const mensagensDeErro = {
 
 const validadores = {
     dataNascimento: input => validaDataNascimento(input)
+}
+
+function mostraMensagemDeErro(tipoDeInput, input){
+    let mensagem = ''
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]){
+            mensagem = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+    return mensagem
 }
 
 function validaDataNascimento(input) {
