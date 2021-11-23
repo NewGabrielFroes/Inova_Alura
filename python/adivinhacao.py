2d2284de019100cfbe94c98da0df3021d2bf99b5
Gabriel Froes <gabriel.froes32@gmail.com>
2021-11-23 10:32:08 -0300

    Iniciando novo módulo de python.

---
+++ python/adivinhacao.py
@@ -0,0 +1,17 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("Curso Alura, Python 3 parte 1")
+print("*********************************")
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu número: ")
+print("Você digitou: ", chute_str)
+chute = int(chute_str)
+
+if (numero_secreto == chute):
+    print("Você acertou!")
+else:
+    print("Você errou!")
+
+print("Fim do jogo")
