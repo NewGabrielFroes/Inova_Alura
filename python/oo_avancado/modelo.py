272a21ecc872c5891945ba89b65b72fd10d3331c
Gabriel Froes <gabriel.froes32@gmail.com>
2021-11-25 11:53:45 -0300

    quando não usar herença

--- python/oo_avancado/modelo.py
+++ python/oo_avancado/modelo.py
@@ -22,6 +22,7 @@ class Programa:
     def __str__(self):
         return f'Nome: {self.nome} Likes: {self.likes}'
 
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
@@ -29,7 +30,6 @@ class Filme(Programa):
 
     def __str__(self):
         return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'
-
 
 
 class Serie(Programa):
@@ -40,16 +40,40 @@ class Serie(Programa):
     def __str__(self):
         return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
 
+
+class Playlist():
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
-vingadores.dar_likes()
-vingadores.dar_likes()
-vingadores.dar_likes()
+tmep = Filme('todo mundo em panico', 1999, 100)
+demolidor = Serie('demolidor', 2016, 2)
 
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
 
-lista = [atlanta, vingadores]
+lista = [atlanta, vingadores, demolidor, tmep]
+minha_playlist = Playlist('fim de semana', lista)
 
-for programa in lista:
+for programa in minha_playlist.listagem:
     print(programa)
+
+print(f'Tamanho: {len(minha_playlist.listagem)}')
