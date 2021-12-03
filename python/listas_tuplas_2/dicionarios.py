bf3fefa9b658a39971702e4d39d988e4ac614008
Gabriel Froes <gabriel.froes32@gmail.com>
2021-12-03 16:55:46 -0300

    utilizando o Counter

--- python/listas_tuplas_2/dicionarios.py
+++ python/listas_tuplas_2/dicionarios.py
@@ -60,3 +60,55 @@ for chave, valor in aparicoes.items():
   print(chave, "=", valor)
 
 print(["palavra {}".format(chave) for chave in aparicoes.keys()])
+
+meu_texto = "Bem vindo meu nome é Guilherme eu gosto muito de nomes e tenho o meu cachorro e gosto muito de cachorro"
+meu_texto = meu_texto.lower()
+
+aparicoes = {}
+
+for palavra in meu_texto.split():
+  ate_agora = aparicoes.get(palavra, 0)
+  aparicoes[palavra] = ate_agora + 1
+
+print(aparicoes)
+
+from collections import defaultdict
+
+aparicoes = defaultdict(int)
+
+for palavra in meu_texto.split():
+  ate_agora = aparicoes[palavra]
+  aparicoes[palavra] = ate_agora + 1
+
+
+int()
+
+dicionario = defaultdict(int)
+print(dicionario['guilherme'])
+
+dicionario['guilherme'] = 15
+print(dicionario['guilherme'])
+
+aparicoes = defaultdict(int)
+
+for palavra in meu_texto.split():
+  aparicoes[palavra] += 1
+
+
+class Conta:
+  def __init__(self):
+    print("Criando uma conta")
+
+contas = defaultdict(Conta)
+
+
+from collections import Counter
+
+aparicoes = Counter()
+for palavra in meu_texto.split():
+  aparicoes[palavra] += 1
+
+
+aparicoes = Counter(meu_texto.split())
+
+print(aparicoes)
