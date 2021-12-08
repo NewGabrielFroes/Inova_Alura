11469738fd9883ea125ce7ed2fb791e725661ce3
Gabriel Froes <gabriel.froes32@gmail.com>
2021-12-08 17:10:42 -0300

    trabalhando com testes

---
+++ python/testes_automatizados/test_dominio.py
@@ -0,0 +1,48 @@
+from dominio import Usuario, Lance, Leilao, Avaliador
+from unittest import TestCase
+
+
+
+class TestAvaliador(TestCase):
+
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
+        gui = Usuario('Gui')
+        yuri = Usuario('Yuri')
+
+        lance_do_yuri = Lance(yuri, 100.0)
+        lance_do_gui = Lance(gui, 150.0)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_do_yuri)
+        leilao.lances.append(lance_do_gui)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 150.0
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
+        yuri = Usuario('Yuri')
+        gui = Usuario('Gui')
+
+        lance_do_gui = Lance(gui, 150.0)
+        lance_do_yuri = Lance(yuri, 100.0)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_do_gui)
+        leilao.lances.append(lance_do_yuri)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 150.0
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
