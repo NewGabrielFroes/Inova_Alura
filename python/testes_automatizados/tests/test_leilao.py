8b29b120815d23c56fcf6aafb4c2059b1fd66020
Gabriel Froes <gabriel.froes32@gmail.com>
2021-12-10 15:31:04 -0300

    módulo finalizado

--- python/testes_automatizados/tests/test_leilao.py
+++ python/testes_automatizados/tests/test_leilao.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from testes_automatizados.src.dominio import Usuario, Lance, Leilao
+from testes_automatizados.tests.excecoes import LanceInvalido
 
 
 class TestAvaliador(TestCase):
@@ -23,7 +24,7 @@ class TestAvaliador(TestCase):
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
     def test_nao_deve_permitir_propor_um_lance_em_ordem_decrescente(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(LanceInvalido):
             yuri = Usuario('Yuri', 500.0)
             lance_do_yuri = Lance(yuri, 100.0)
 
@@ -74,6 +75,6 @@ class TestAvaliador(TestCase):
     def test_nao_deve_permitir_propor_lance_caso_o_usuario_seja_o_mesmo(self):
         lance_do_gui200 = Lance(self.gui, 200.0)
 
-        with self.assertRaises(ValueError):
+        with self.assertRaises(LanceInvalido):
             self.leilao.propoe(self.lance_do_gui)
             self.leilao.propoe(lance_do_gui200)
--- python/testes_automatizados/tests/test_usuario.py
+++ python/testes_automatizados/tests/test_usuario.py
@@ -1,6 +1,9 @@
 from testes_automatizados.src.dominio import Usuario, Leilao
 import pytest
 
+from testes_automatizados.tests.excecoes import LanceInvalido
+
+
 @pytest.fixture
 def vini():
     return Usuario('Vini', 100.0)
@@ -26,5 +29,5 @@ def test_deve_permitir_propor_lance_quando_o_valor_eh_igual_ao_valor_da_carteira
 
 def test_nao_deve_permitir_propor_lance_com_valor_maior_que_o_da_carteira(vini, leilao):
 
-    with pytest.raises(ValueError):
+    with pytest.raises(LanceInvalido):
         vini.propoe_lance(leilao, 200.0)
