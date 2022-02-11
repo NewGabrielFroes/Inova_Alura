aed4ac1989a5ab57a8c3d539a3caf0f09f21e393
NewGabrielFroes <gabriel.froes32@gmail.com>
2022-02-11 16:27:54 -0300

    criando lista de categoria

---
+++ react/react_ciclo_componentes/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      notas:[]
+    }
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota];
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado);
+  }
+
+  deletarNota(index){
+    let arrayNotas = this.state.notas
+    arrayNotas.splice(index,1)
+    this.setState({notas:arrayNotas})
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <main>
+          <ListaDeNotas 
+          apagarNota = {this.deletarNota.bind(this)}
+          notas={this.state.notas}/> 
+        </main>
+      </section>
+    );
+  }
+}
+
+//new ListaDeNotas({notas:this.notas})
+export default App;
